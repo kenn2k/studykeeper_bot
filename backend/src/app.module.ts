4b3c72108059ea25a5b6bfc6ce01220052bf76b2
Shkreba Anton <antonshkreba.a@gmail.com>
2024-09-10 01:06:57 +0300

    fix: prisma schema reserved name

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,8 +6,16 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
 import { BotModule } from './bot/bot.module';
+import { AppController } from './app/app.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule, TaskModule, BotModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    AuthModule,
+    TaskModule,
+    BotModule,
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
